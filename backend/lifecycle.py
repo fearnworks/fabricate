74381662a7a387295c3ec74501f1eae24b0de0c4
jphillips <josh.phillips@fearnworks.com>
2023-12-08 15:53:44 -0600

    Add toggle for model load

--- backend/lifecycle.py
+++ backend/lifecycle.py
@@ -1,12 +1,14 @@
 # lifecycle.py
 
 from loguru import logger
-from model_manager import start_model_loading, await_model_loading
+from model_manager import start_model_loading
 
 
-async def startup():
-    logger.info("Application startup: Model loading in background...")
-    start_model_loading()
+async def startup(load_model=False):
+    logger.info("Application startup: ")
+    if load_model:
+        logger.info("Model loading in background...")
+        start_model_loading()
 
 async def shutdown():
     logger.info("Application shutdown: Cleaning up resources.")
