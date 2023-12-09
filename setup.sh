4700986acbe9c12e5a807d628a446a94cf00a389
charlie.rogers <charlie.rogers@fearnworks.com>
2023-12-09 15:51:32 -0600

    changed setup to allow network creation, and added config path to gitignore

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-npm install -g @vue/cli
-git clone https://github.com/SkunkworksAI/BakLLaVA.git ./backend/BakLLaVApip i
+# Network name
+NETWORK_NAME="fabricate-network"
+
+# Check if the network exists
+if [ -z "$(docker network ls --filter name=^${NETWORK_NAME}$ --format '{{.Name}}')" ]; then
+    echo "Network ${NETWORK_NAME} does not exist. Creating it..."
+    docker network create ${NETWORK_NAME}
+else
+    echo "Network ${NETWORK_NAME} already exists."
+fi
+
+# Run Docker Compose
+docker compose up
